c81a1953179394ab80f57922d2eb73f2e37e7927
Indre Surviliene <indre.bitinaite@gmail.com>
2022-03-31 20:49:26 +0300

    new changes

---
+++ SuperHero/Hero.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperHero
+{
+    public enum HeroType
+    {
+        Hero,
+        Villain,
+    }
+
+    internal class Hero
+    {
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public string Nickname { get; set; }
+
+        public int HeroID { get; set; }
+
+        public HeroType Type { get; set; }
+
+        public int DeedTime { get; set; }
+
+        public Hero()
+        {
+            Name = "John";
+            Surname = "Smith";
+            Nickname = "Powerhero";
+            HeroID = 1111;
+            Type = HeroType.Hero;
+            DeedTime = 10;
+        }
+        public Hero (string Name, string Surname, string Nickname, int HeroID, HeroType Hero, int DeedTime)
+        {
+            this.Name = Name;
+            this.Surname = Surname;
+            this.Nickname = Nickname;
+            this.HeroID = HeroID;
+            this.Type = Hero;
+            this.DeedTime = DeedTime;
+        }
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Name: {Name}\n Surname: {Surname}\n Nickname: {Nickname}\n HeroID: {HeroID}\n Type: {Type}\n Deedtime: {DeedTime}");
+        }
+
+        public int CalculatedLevel()
+        {
+            int level = 0;
+            if (DeedTime < 20)
+            {
+                level = 1;
+            }
+            if (DeedTime >= 20 && DeedTime <= 40)
+            {
+                level = 2;
+            }
+            if (DeedTime > 40)
+            {
+                level = 3;
+            }
+
+            return level;
+        }
+    }
+}
--- SuperHero/Program.cs
+++ SuperHero/Program.cs
@@ -8,46 +8,185 @@ namespace SuperHero
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) 
         {
-           /* string name = "PowerHero";
-            int age = 33;
-            string power1 = "fast runner";
-            string power2 = "good swimmer";
-            string power3 = "strong body";
-
-            Console.WriteLine("********************");
-            Console.WriteLine($"Hero: {name}");
-            Console.WriteLine($"Age: {age}");
-            Console.WriteLine($"Superpowers: {power1}, {power2}, {power3}");
-            Console.WriteLine("********************");
-
-            Console.WriteLine("***************FINANCIAL INFO******************");
-
-            int deedTimeInHours1 = 10;
-            int deedTimeInHours2 = 20;
-            int deedTimeInHours3 = 30;
-
-            int result = deedTimeInHours1 + deedTimeInHours2 + deedTimeInHours3; // - Total time spent on deeds
-            Console.WriteLine($"Total time spend on deed: {result} hours");
-
-            int result2 = result / 3; // - Average time spent on one deed
-            Console.WriteLine($"Average time spend on deed: {result2} hours");
-
-            double salary = 2000; //monthly
-            double cookieCost = 2;
-            double hour = 24;
-            bool isEvil = false;
-
-            double dailySalary = Math.Round(salary / 30, 2); //-- monthlySalary
-            Console.WriteLine($"Hero earns daily {dailySalary} Eur");
-
-            double boughtCookies = Math.Floor(salary / cookieCost); //-- Add boughtCookies calculation
-            double gotCookiesPerHour = Math.Floor(boughtCookies / hour); //- How many cookies Hero will get. 5 cookies per hour
-            Console.WriteLine($"The hero gets {boughtCookies} cookies");
-            Console.WriteLine($"The hero gets {gotCookiesPerHour} cookies per hour");
             
+            Hero Powerhero = new Hero("John", "Smith", "Powerhero", 1111, HeroType.Hero, 10);
+            Hero Spiderman = new Hero("Ben", "Snow", "Spiderman", 2222, HeroType.Hero, 15);
+            Hero Batman = new Hero("Will", "White", "Batman", 3333, HeroType.Villain, 20);
+            Hero Superwoman = new Hero("Ann", "Rose", "Superwoman", 4444, HeroType.Hero, 25);
 
+            Powerhero.PrintInfo();
+            Spiderman.PrintInfo();
+            Batman.PrintInfo();
+            Superwoman.PrintInfo();
+
+            List<Hero> listOfHeroes = new List<Hero>();
+            listOfHeroes.Add(Powerhero);
+            listOfHeroes.Add(Spiderman);
+            listOfHeroes.Add(Batman);  
+            listOfHeroes.Add(Superwoman);
+
+            District antakalnis = new District ("Antakalnis", "Vilnius", 01, listOfHeroes);
+
+            /*for (int i = 0; i < metropole.Count; i++)
+            {
+                Console.WriteLine(metropole[i].Nickname);
+            }*/
+            
+            //metropole.RemoveAt(0);
+
+            var foundHero = listOfHeroes.Find(find => find.Name == "John");
+            Console.WriteLine($"We found {foundHero.Name}");
+            foundHero.PrintInfo(); //Prints all info about John
+
+            //var foundHero2 = metropole.Find(find => find.CalculatedLevel == 1); //doesn't work
+
+
+            string[] superHeroNameList = { "Powerhero", "Spiderman", "Batman", "Superwoman" };
+            string[] superHeroAgeList = { "33", "27", "45", "23" };
+            string[] superHeroPowerList = { "Good swimmer, fast runner, strong body", "Good climber, not affraid of height, jumps well", "Flies high, fast, furious", "Knows everything, does everything, can code" };
+
+
+            double[,] deedTimeInHours2D = { { 10, 20, 30 }, { 15, 25, 35 }, {20, 30, 40 }, {25, 35, 45} };
+            double[] deedTimeInHoursList = new double[superHeroNameList.Length];
+            double[] averageDeedTimeList = new double[superHeroNameList.Length];
+
+            for (int d = 0; d < superHeroNameList.Length; d++)
+            {
+               deedTimeInHoursList[d] = deedTimeInHours2D[d, 0] + deedTimeInHours2D[d, 1] + deedTimeInHours2D[d, 2];
+               averageDeedTimeList[d] = deedTimeInHoursList[d] / 3;
+            }
+
+            double[] salary = { 2000, 3000, 4000, 5000 };
+            double[] dailySalaryList = new double[superHeroNameList.Length];
+        
+            for (int s = 0; s < salary.Length; s++) 
+            {
+                dailySalaryList[s] = Math.Round(salary[s] / 30, 2);
+            }
+
+            double cookies = 2;
+            double hour = 24;
+
+            double[] boughtCookiesList = new double[superHeroNameList.Length];
+            double[] gotCookiesPerHourList = new double[superHeroNameList.Length];
+
+            for (int c = 0; c < salary.Length; c++)
+            {
+                boughtCookiesList[c] = Math.Floor(salary[c] / cookies);
+                gotCookiesPerHourList[c] = Math.Floor(boughtCookiesList[c] / hour);
+            }
+ 
+
+            bool isMenuRunning = true;
+            do
+            {
+                string menuItems;
+
+                Console.WriteLine($"Welcome to the superhero application!");
+                Console.WriteLine($"Select what to do:");
+                Console.WriteLine($"1 - Show a list of superheroes");
+                Console.WriteLine($"2 - Show specific hero");
+                Console.WriteLine($"3 - Adding a superhero");
+                Console.WriteLine($"4 - Deleting a superhero");
+                Console.WriteLine($"5 - OOP hero");
+                Console.WriteLine($"6 - Exit");
+                Console.WriteLine($"7 - Information about district");
+
+                menuItems = Console.ReadLine();
+
+                switch (menuItems)
+                {
+                    case "1":
+                        Console.WriteLine("============List=of=superheroes============");
+                        for (int i = 0; i < superHeroNameList.Length; i++)
+                        {
+                            Console.WriteLine($"{i}. {superHeroNameList[i]}");
+                        }
+                        Console.WriteLine("===========================================");
+                        break;
+
+                    case "2":
+                        Console.WriteLine($"Please choose a superhero by number");
+                        for (int i = 0; i < superHeroNameList.Length; i++)
+                        {
+                            Console.WriteLine($"{i}. {superHeroNameList[i]}");
+                        }
+                        int.TryParse(Console.ReadLine(), out int chosenNumber);
+                        Console.WriteLine($"You have chosen {superHeroNameList[chosenNumber]}");
+
+                        Console.WriteLine("Choose what type of info to show");
+                        Console.WriteLine($"1 - GENERAL INFO");
+                        Console.WriteLine($"2 - FINANCIAL INFO");
+                        string showMenu = Console.ReadLine();
+
+
+                        if (showMenu == "1")
+                        {
+                            Console.WriteLine("*************GENERAL INFO********************");
+                            Console.WriteLine($"Hero: {superHeroNameList[chosenNumber]}");
+                            Console.WriteLine($"Age: {superHeroAgeList[chosenNumber]}");
+                            Console.WriteLine($"Superpowers: {superHeroPowerList[chosenNumber]}");
+                            Console.WriteLine("********************************** \n \n");
+                        }
+                        else if (showMenu == "2")
+                        {
+                            Console.WriteLine("**************FINANCIAL INFO*****************");
+
+                            Console.WriteLine($"Time spent on deeds: {deedTimeInHoursList[chosenNumber]}");
+                            Console.WriteLine($"Average time spend on deed: { averageDeedTimeList[chosenNumber]} hours");
+                            Console.WriteLine($"Hero earns daily: {dailySalaryList[chosenNumber]} Eur");
+                            Console.WriteLine($"The hero gets {boughtCookiesList[chosenNumber]} cookies");
+                            Console.WriteLine($"The hero gets {gotCookiesPerHourList[chosenNumber]} cookies per hour \n");
+                        }
+                        break;
+                    case "3":
+                        Console.WriteLine("What is the new superhero name?");
+                        string superHeroName = Console.ReadLine();
+                        superHeroNameList = superHeroNameList.Append(superHeroName);
+                        Console.WriteLine($"SuperHero {superHeroName} Added!");
+                        break;
+                case "4":
+                        Console.WriteLine($"Select a hero you want to delete");
+                        
+                        for (int i = 0; i < superHeroNameList.Length; i++)
+                        {
+                            Console.WriteLine($"{i}. {superHeroNameList[i]}");
+                        }
+
+                        int.TryParse(Console.ReadLine(), out int chosenNumber2);
+                        superHeroNameList = superHeroNameList.Remove(chosenNumber2);
+                        Console.WriteLine($"You have deleted {chosenNumber2} hero");
+                        break; 
+                case "5": 
+                        //Console.WriteLine($"{hero.Name} level is {hero.CalculatedLevel()}");
+
+                        foreach (Hero hero in listOfHeroes)
+                        {
+                            if (hero.CalculatedLevel() > 1)
+                            {
+                                Console.WriteLine($"{hero.Nickname} is higher than level 1");
+                            }
+                            Console.WriteLine($"The average of level of heroes inside {antakalnis.Title} is: {antakalnis.CalculateAvgLevelInDistrict()}");  
+                        }
+                        break; 
+                    case "6":
+                        isMenuRunning = false;
+                        Console.WriteLine($"Good bye!");
+                        break;
+                    case "7":
+                        antakalnis.PrintInformationAboutDistrict();
+                        break;
+                    default:
+                    Console.WriteLine($"Please choose from the available menu");
+                    break;
+            }
+            } 
+            while (isMenuRunning);
+
+            
+            bool isEvil = false;
             if (!isEvil) 
             {
                 Console.WriteLine("The Hero is not evel and gets his salary");
@@ -56,8 +195,9 @@ namespace SuperHero
             {
                 Console.WriteLine("The hero is evel and doesn't get his salary");
             }
-            //--isEvil or not
-*/
+
+           
+
             char deedGrade = 'A';
             Console.WriteLine("Enter a letter from A to H");
             string charString = Console.ReadLine();
@@ -80,7 +220,7 @@ namespace SuperHero
                     Console.WriteLine("Bad, you are true villain");
                     break;
             }
-
+         
                 
 
 
